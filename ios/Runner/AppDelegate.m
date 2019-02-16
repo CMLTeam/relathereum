081cda9ec52ccc94ead7ff0cb6031980cdfb7e8b
Oleksandr Shchetynin <oleksandr.shchetynin@gmail.com>
2019-02-16 21:15:16 +0100

    iOS fixes

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -4,12 +4,10 @@
 
 @implementation AppDelegate
 
-    - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-[GMSServices provideAPIKey:@"AIzaSyAZlNFjWYqHW7SL0hDysQi_M_9lOg-G2gY"];
-
-[GeneratedPluginRegistrant registerWithRegistry:self];
-// Override point for customization after application launch.
-    return [super application:application didFinishLaunchingWithOptions:launchOptions];
+- (BOOL)application:(UIApplication *)application
+    didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  [GMSServices provideAPIKey:@"AIzaSyAZlNFjWYqHW7SL0hDysQi_M_9lOg-G2gY"];
+  [GeneratedPluginRegistrant registerWithRegistry:self];
+  return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
-
 @end
