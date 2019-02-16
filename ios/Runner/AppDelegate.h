697c66accddd8c269f691218f49fd3468b234140
korzin <Samkorzin@gmail.com>
2019-02-16 19:24:16 +0200

    Fix ios maps

--- ios/Runner/AppDelegate.h
+++ ios/Runner/AppDelegate.h
@@ -1,6 +1,19 @@
+#include "AppDelegate.h"
+#include "GeneratedPluginRegistrant.h"
 #import <UIKit/UIKit.h>
 #import <Flutter/Flutter.h>
+#import "GoogleMaps/GoogleMaps.h"
 
 @interface AppDelegate : FlutterAppDelegate
 
+@end
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application
+    didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  [GMSServices provideAPIKey:@"AIzaSyAZlNFjWYqHW7SL0hDysQi_M_9lOg-G2gY"];
+  [GeneratedPluginRegistrant registerWithRegistry:self];
+  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+}
 @end
