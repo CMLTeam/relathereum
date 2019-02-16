7667f8182c0a5262db7a12db84f3dfa9cd936a09
romakatsa <romakatsa@gmail.com>
2019-02-16 16:41:51 +0200

    web3 try

--- lib/screens/Profile/index.dart
+++ lib/screens/Profile/index.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import 'package:flutter_flat_app/components/services/web3Service.dart';
 
 
 class ExampleScreen extends StatefulWidget {
@@ -8,7 +9,21 @@ class ExampleScreen extends StatefulWidget {
   ExampleState createState() => new ExampleState();
 }
 
+
 class ExampleState extends State<ExampleScreen> {
+
+  BuildContext context;
+  Web3Service web3service = new Web3Service();
+
+  initState() {
+    super.initState();
+    getGem();
+  }
+
+  getGem() async {
+    this.web3service.test();
+  }
+
   @override
   Widget build(BuildContext context) {
     return  Text('Example Screen. Feel free to adjust.', style: TextStyle(fontSize: 20));
