046f1779d44b9ba20cb5fa140030c196b2c2cf0c
xonix <xonixx@gmail.com>
2019-02-16 16:29:40 +0200

    truffle project

--- solidity/migrations/1_initial_migration.js
+++ solidity/migrations/1_initial_migration.js
@@ -1,5 +1,7 @@
 var Migrations = artifacts.require("./Migrations.sol");
+var CapsuleEscrow = artifacts.require("./CapsuleEscrow.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Migrations);
+  deployer.deploy(CapsuleEscrow);
 };
