f771b425fbf9f0f0e1422e3f49e89cae59d54b19
korzin <Samkorzin@gmail.com>
2019-02-16 19:31:14 +0200

    Fix ios maps 3

--- ios/Runner/AppDelegate.h
+++ ios/Runner/AppDelegate.h
@@ -5,10 +5,5 @@
 #import "GoogleMaps/GoogleMaps.h"
 
 @interface AppDelegate : FlutterAppDelegate
-- (BOOL)application:(UIApplication *)application
-    didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-  [GMSServices provideAPIKey:@"AIzaSyAZlNFjWYqHW7SL0hDysQi_M_9lOg-G2gY"];
-  [GeneratedPluginRegistrant registerWithRegistry:self];
-  return [super application:application didFinishLaunchingWithOptions:launchOptions];
-}
+
 @end
--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -4,6 +4,8 @@
     @implementation AppDelegate
 
     - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+[GMSServices provideAPIKey:@"AIzaSyAZlNFjWYqHW7SL0hDysQi_M_9lOg-G2gY"];
+
 [GeneratedPluginRegistrant registerWithRegistry:self];
 // Override point for customization after application launch.
     return [super application:application didFinishLaunchingWithOptions:launchOptions];
