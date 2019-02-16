286e5d590dc28c892881bcb218da488cec21a239
xonix <xonixx@gmail.com>
2019-02-16 18:19:51 +0200

    flutter to ethereum

--- lib/components/services/web3Service1.dart
+++ lib/components/services/web3Service1.dart
@@ -5,6 +5,7 @@ import 'package:flutter/services.dart';
 import 'package:http/http.dart';
 import 'package:path/path.dart';
 import 'package:web3dart/web3dart.dart';
+
 //import '/assets/abi/GemERC721.json' as gemContractAbi;
 //const gemContractAbi = './assets/abi/GemERC721.json';
 const abi = './assets/abi/contracts_Test_sol_Test.abi';
@@ -13,12 +14,12 @@ const abi = './assets/abi/contracts_Test_sol_Test.abi';
 //const String API_KEY = "https://rinkeby.infura.io/v3/6a35db15e00e4a8188c4b031236afa8f";
 //const String GEM_ERC721 = "0x82ff6bbd7b64f707e704034907d582c7b6e09d97";
 
-const String _PRIVATE_KEY = "1f7d5adba7872a45a4d98171b80728865a3b40526b5cac73667b3e78787d9ad2";
+const String _PRIVATE_KEY =
+    "1f7d5adba7872a45a4d98171b80728865a3b40526b5cac73667b3e78787d9ad2";
 const String API_URL = "https://tunnel2.cmlteam.com";
-const String CONTRACT_ADDRESS = "0x719d265c3f5bd4abcf45a2f3ebe96db1a4b18a48";
+const String CONTRACT_ADDRESS = "0x32fbec8987e9af48f6e7b02256e567566b42d934";
 
 class Web3Service1 {
-
   Future<Null> test() async {
     var httpClient = new Client();
     Web3Client client = new Web3Client(API_URL, httpClient);
@@ -28,22 +29,32 @@ class Web3Service1 {
 
     String jsonContent = await rootBundle.loadString(abi);
 
-    var gemContractABI = ContractABI
-        .parseFromJSON(jsonContent, "Test");
+    var contractABI = ContractABI.parseFromJSON(jsonContent, "Test");
 
-    var gemContract = new DeployedContract(
-        gemContractABI, new EthereumAddress(CONTRACT_ADDRESS), client, credentials);
+    var contract = new DeployedContract(contractABI,
+        new EthereumAddress(CONTRACT_ADDRESS), client, credentials);
 
-    var fn = gemContract
-        .findFunctionsByName("add")
-        .first;
+    var fnAdd = contract.findFunctionsByName("add").first;
+    var fnSetval = contract.findFunctionsByName("setval").first;
+    var fnGetval = contract.findFunctionsByName("getval").first;
 
-    var gemOwnerResponce = await new Transaction(
-        keys: credentials, maximumGas: 80000)
-        .prepareForCall(gemContract, fn, [BigInt.from(2), BigInt.from(5)])
-        .call(client);
+    var resp = await new Transaction(keys: credentials, maximumGas: 80000)
+        .prepareForCall(
+            contract, fnAdd, [BigInt.from(2), BigInt.from(5)]).call(client);
 
-    var res = gemOwnerResponce.toString();
+    var res = resp.toString();
     print("RES is: $res");
+
+    resp = await new Transaction(keys: credentials, maximumGas: 80000)
+        .prepareForCall(contract, fnSetval, [BigInt.from(5)]).send(client);
+
+    res = resp.toString();
+    print("RES1 is: $res");
+
+    resp = await new Transaction(keys: credentials, maximumGas: 80000)
+        .prepareForCall(contract, fnGetval, []).call(client);
+
+    res = resp.toString();
+    print("RES2 is: $res");
   }
 }
--- solidity/contracts/Test.sol
+++ solidity/contracts/Test.sol
@@ -1,7 +1,17 @@
 pragma solidity ^0.4.23;
 
 contract Test {
+    uint v;
+
     function add(uint a, uint b) pure public returns (uint) {
         return a + b;
+    }
+
+    function setval(uint _v) public {
+        v = _v + 1;
+    }
+
+    function getval() view public returns (uint) {
+        return v;
     }
 }
