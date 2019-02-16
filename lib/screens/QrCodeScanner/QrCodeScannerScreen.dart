eaa87b5207c5fa3fba02628631ae71df74860e6b
Alexey Kopylov <tonn.tamerlan@gmail.com>
2019-02-16 21:11:38 +0200

    rfct

--- lib/screens/QrCodeScanner/QrCodeScannerScreen.dart
+++ lib/screens/QrCodeScanner/QrCodeScannerScreen.dart
@@ -48,40 +48,12 @@ class _ScanState extends State<QrCodeScannerScreen> {
 
 
     return Scaffold(
-        body: Container(
-            color: Colors.white,
-            child: Column(children: [buildTitle(), screen])),
+        body: addTitleToScreen(screen),
         floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
         floatingActionButton: ScanQrButton(
           scanQr: scan, isTransparent: true,
         ));
   }
-
-  someWidget() => Scaffold(
-          body: new Center(
-        child: new Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-              child: RaisedButton(
-                  color: Colors.blue,
-                  textColor: Colors.white,
-                  splashColor: Colors.blueGrey,
-                  onPressed: scan,
-                  child: const Text('START CAMERA SCAN')),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-              child: Text(
-                "dsfdsfd",
-                textAlign: TextAlign.center,
-              ),
-            ),
-          ],
-        ),
-      ));
 
   scan() async {
     try {
