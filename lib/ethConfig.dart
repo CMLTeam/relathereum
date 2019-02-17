f617c651fade1be706c386a9720395b649a87054
xonix <xonixx@gmail.com>
2019-02-17 03:05:51 +0200

    adjust for prod setup

--- lib/ethConfig.dart
+++ lib/ethConfig.dart
@@ -2,5 +2,6 @@ const String ETH_PRIVATE_KEY =
     "0x0000000000000000000000000000000000000000000000000000000000000007";
 //const String ETH_PRIVATE_KEY =
 //    "1f7d5adba7872a45a4d98171b80728865a3b40526b5cac73667b3e78787d9ad2";
-const String ETH_API_URL = "https://tunnel2.cmlteam.com";
+//const String ETH_API_URL = "https://tunnel2.cmlteam.com";
+const String ETH_API_URL = "http://cmlteam.com:7545";
 const MAX_GAS = 1000000;
